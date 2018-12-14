aa2e690c083917637bc4428e931e0b715dfa82d5
arf111 <krsultan069@gmail.com>
2018-12-14 23:33:13 +0600

    mnist coding done..training left..

--- Image Classification/mnist_classification.py
+++ Image Classification/mnist_classification.py
@@ -11,13 +11,15 @@ class ConvNet(nn.Module):
         self.conv2 = nn.Conv2d(10,20,3)
         self.conv3 = nn.Conv2d(20,30,3)
         self.conv3drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(250,50)
+        self.fc1 = nn.Linear(14520,50)
         self.fc2 = nn.Linear(50,10)
     
     def forward(self, x):
         x = F.relu(self.conv1(x))
-        x = F.relu( self.conv3drop(self.conv3( self.conv2(x) ) ) )
-        x = x.view(-1,250)
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = F.relu( self.conv3drop(x) )
+        x = x.view(-1,14520)
         x = F.relu(self.fc1(x))
         x = F.dropout(x)
         x = self.fc2(x)
@@ -32,7 +34,7 @@ def train(model,device,train_loader,optimizer,criterion,epoch):
         
         optimizer.zero_grad()
 
-        output = model(data)
+        output = model(data) # [1,10]
         loss = criterion(output, target)
         loss.backward()
         optimizer.step()
@@ -40,23 +42,37 @@ def train(model,device,train_loader,optimizer,criterion,epoch):
         if batch_idx % 10 == 0:
             print('Train epoch: ',epoch,' loss:',loss.item())
 
+def test(model,device,test_loader,criterion,epoch):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += criterion(output,target).item()
+            prediction = output.max(1, keepdim=True)[1]
+            correct += prediction.eq(target.view_as(prediction)).sum().item()
+    
+    test_loss /= len(test_loader.dataset)
+
+    print('\n Avg loss: ',test_loss,' Accuracy {}/{}'.format(correct,len(test_loader.dataset)),' ({:,0f}%)'.format(100 * correct / len(test_loader.dataset)))
+
         
-
-
 def main():
     train_loader = torch.utils.data.DataLoader(
         datasets.MNIST(
-            '../data',download=True,
+            'data',download=True,
             transform=transforms.Compose([
                 transforms.ToTensor(),
-                transforms.Normalize(0.5,0.5)
+                transforms.Normalize((0.5,),(0.5,))
             ])))
     
     test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data',train=False,
+        datasets.MNIST('data',train=False,
         transform=transforms.Compose([
                 transforms.ToTensor(),
-                transforms.Normalize(0.5,0.5)            
+                transforms.Normalize((0.5,),(0.5,))            
             ])))
     
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -66,9 +82,10 @@ def main():
     optimizer = optim.Adam(model.parameters(), lr=0.001)
     criterion = nn.NLLLoss()
 
-    for epoch in range(1, 10):
+    for epoch in range(1, 5):
         train(model,device,train_loader,optimizer,criterion,epoch)
-
+        test(model,device,test_loader,criterion,epoch)
+    
 if __name__ == '__main__':
     main()
         
